732cd8c540a5ce04ade4891b5147d2976f515791
Luiz H. S. Bispo <luizh3bispo@gmail.com>
2022-10-19 15:18:43 -0300

    final upload + update

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -42,5 +42,65 @@ namespace MVC.Controllers
             }
             return View(contato);
         }
+
+        //método para criar pagina editar contato, busca Id que recebeu para editar
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return NotFound();
+            //se usar RedirectToAction(nameof(Index)) em vez de NotFound,
+            //ele não retorna erro e sim retorna a página original
+            return View(contato);
+        }
+        // método para editar o contato
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find((contato.Id));
+            //recebe variavel contatoBanco para editar o contato
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+            //atualiza o contato com a opção update e saveChanges
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+            //atualiza a página com o contato editado
+            return RedirectToAction(nameof(Index));
+        }
+
+        //metodo para criar pagina de detalhes
+        public IActionResult Detalhes(int id)
+        {    //variavel para procurar pelo ID
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+            //se variavel for null retorna pra pagina original (não retorna erro)
+            return View(contato);
+        }
+
+        //metodo para pagina de deletar contato
+        public IActionResult Deletar(int id)
+        {    //variavel para procurar pelo ID
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+            //se variavel for null retorna pra pagina original (não retorna erro)
+            return View(contato);
+        }
+
+        //metodo para deletar o contato
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+            //busca a variavel pela ID e remove ela
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+            //retorna para a pagina da tabela de contatos
+            return RedirectToAction(nameof(Index));
+
+
+        }
     }
 }
